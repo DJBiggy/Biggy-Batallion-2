28d06fc2a8e2f998205c958e9778df8521040399
Juanathan Bean <randot771@gmail.com>
2023-09-14 13:14:00 -0400

    Initial Commit

---
+++ Biggy Batallion 2UITests/Biggy_Batallion_2UITestsLaunchTests.swift
@@ -0,0 +1,32 @@
+//
+//  Biggy_Batallion_2UITestsLaunchTests.swift
+//  Biggy Batallion 2UITests
+//
+//  Created by 1a on 9/14/23.
+//
+
+import XCTest
+
+final class Biggy_Batallion_2UITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
